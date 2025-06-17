5602f944b59d9b2a818af2c1f5c741f7075cac92
EduardoLeal1985 <eduleal2005@gmail.com>
2025-06-17 15:16:35 -0300

    day 2 - react native guide

--- screens/HomeScreen/index.tsx
+++ screens/HomeScreen/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet, Button } from 'react-native';
 import BookCard from '../../components/BookCard';
 import { books } from '../../data/books';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../routes';
-import { Container } from './styles';
+import { Container, TopMenu } from './styles';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 export default function HomeScreen({ navigation }: Props) {
     return (
         <Container>
+            <TopMenu><Button title="Sobre" onPress={() => navigation.navigate('About')} /></TopMenu>
             <FlatList
                 data={books}
                 keyExtractor={(item) => item.id}
--- screens/HomeScreen/styles.ts
+++ screens/HomeScreen/styles.ts
@@ -4,3 +4,8 @@ export const Container = styled.SafeAreaView`
   flex: 1;
   padding: 16px;
 `;
+
+export const TopMenu = styled.View`
+  margin-bottom: 16px;
+  align-items: flex-end;
+`;
